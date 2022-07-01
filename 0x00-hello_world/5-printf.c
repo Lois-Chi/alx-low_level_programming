cc2a09d9a41e93fed7ddb52f2f0f967dc5dccb05
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-07-01 02:35:33 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/
-*min - Entry point
-*Return: Always 0 (success)
-*/
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
-}	
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
