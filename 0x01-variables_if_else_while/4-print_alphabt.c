fd136692f0f107b92103448daf807eaf68d4ca37
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-07-02 06:25:35 -0700

    Print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all letters except q and e
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
