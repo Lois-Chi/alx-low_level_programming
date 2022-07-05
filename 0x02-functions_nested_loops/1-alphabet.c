152fe8d2fcf849a6184e8c9935c79c20d70c9cce
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-07-05 08:35:06 -0700

    Alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - prints the alphabets in lowercase
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+
+{
+	_putchar ch;
+
+	for (ch 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	_putchar ('\n');
+	return (0);
+}
