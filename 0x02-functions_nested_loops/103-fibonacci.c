6304469dc4490cd88a971d82779da5113e7f202d
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-07-06 16:17:06 -0700

    Even liber abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints a new termin the Fibonacci sequence
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void);
+{
+	int i = 0, j = 1, next = 0;
+	int sum = 0;
+
+	while (next < 4000000)
+	{
+		next = i + j;
+		i = j;
+		j = next;
+		if (next % 2 == 0)
+			sum += next;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
