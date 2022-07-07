9fb5b2495bfbfffc0ba74643a950f6b729824be8
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-07-07 16:00:36 -0700

    Isuper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks uppercase alphabets
+ * @c: is a character
+ * Return: 0 or 1 based on a condition
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
