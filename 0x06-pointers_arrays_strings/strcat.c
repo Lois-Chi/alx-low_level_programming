72bd19bf7eaf3e658f447e650717f5e6f7fff626
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-07-14 07:33:59 -0700

    Hello world concatenated

--- 0x06-pointers_arrays_strings/strcat.c
+++ 0x06-pointers_arrays_strings/strcat.c
@@ -1,21 +1,20 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * *_strcat - appends the src string to dest string
- * @dest: destination parameter
- * @src: source code parameter
- * Return: dest string
+ * *_strcat - concatenates two strings
+ * @src:
+ * @dest:
+ * Return: Destination
  */
 char *_strcat(char *dest, char *src)
 {
 	int len = 0, a;
-
+	
 	while (dest[len])
 		len++;
 	for (a = 0; src[a] != 0; a++)
 	{
-		dest[len] = src[a];
+		dest[len] = src[len];
 		len += 1;
 	}
 	dest[len] = '\0';
