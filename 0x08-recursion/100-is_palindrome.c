4b0a705b3f9d8dbcb588328ddc7193bb6b6c57c8
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-07-21 14:18:08 -0700

    It is palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks if a string is palindrome
+ * @s: string
+ * Return: 1 or 0, based on a condition
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: string param
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * check_pal - checks for palindrome
+ * @i: iterator param
+ * @len: string length param
+ * @s: string param
+ * Return: 0 or 1
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
