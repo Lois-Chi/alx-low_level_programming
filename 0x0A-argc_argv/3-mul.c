2f17bab4354c810e30efff49883db5a794488bd5
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-07-22 16:35:54 -0700

    Multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,6 +4,8 @@
 /**
  * main - prints the result of the multiplication, followed by a new line
  * assuming that the two numbers and results can be stored as integers
+ * @argv: argument variable
+ * @argc: argument count
  * Return: 0 or 1 based on a condition
  */
 int main(int argc, char *argv[])
