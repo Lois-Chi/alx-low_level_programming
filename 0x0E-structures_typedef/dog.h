9880a2a7ce4004ffdbde629218b21f0f88c03e6c
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-08-02 17:55:00 -0700

    Defining a new type with elements in it

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog: definition of type dog
+ * struct dog - definition of type dog
  * @name: character param
  * @age: float param
  * @owner: another character param
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
