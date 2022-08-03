1da1f0c4823bd1b112bf5ff91c95f99f414189c1
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-08-02 17:37:14 -0700

    Structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog: definition of type dog
+ * @name: character param
+ * @age: float param
+ * @owner: another character param
+ */
 char *name;
 float age;
 char *owner;
+typedef struct dog dog_t;
 #endif
