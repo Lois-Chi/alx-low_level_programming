407ff8e8044c56d92be925a699b995d6c1370218
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-12-05 19:55:51 -0800

    Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,11 +11,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	hashTable = calloc(1, sizeof(hash_table_t));
+	hashTable = malloc(sizeof(hash_table_t));
 	if (hashTable == NULL)
 		return (NULL);
 	hashTable->size = size;
-	hashTable->array = calloc(size, sizeof(hash_node_t **));
+	hashTable->array = malloc(sizeof(hash_node_t *) * size);
 	if (hashTable->array == NULL)
 	{
 		free(hashTable);
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -89,4 +89,5 @@ void shash_table_print(const shash_table_t *ht)
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 void insert(shash_table_t *ht, shash_node_t *new_node);
+
 #endif
